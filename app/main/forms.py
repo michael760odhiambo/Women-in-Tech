692c7510392d999abd3f74f629786fbc843f5ec5
michael <mikelkarije@gmail.com>
2019-09-25 12:14:56 +0300

    addition of data in the main folder to its files

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required
+
+class Form(FlaskForm):
+
+    title = StringField(' title',validators=[Required()])
+    review = TextAreaField('post review', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,15 @@
-from . import views
+from flask import render_template
+from . import main
+
+@main.route('/')
+def index():
+    title='welcome to women Tech site'
+    return """
+    <h1>Hello world!</h1>
+
+    <iframe src="https://www.youtube.com/embed/XebNxUE3ugA" width="890" height="500" frameborder="0" allowfullscreen></iframe>
+    """
+
+@main.route('motive')
+def motive():
+    return render_template("motive.html")
