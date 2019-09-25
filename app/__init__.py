7adb60da0ee4daa83239f28fe15d3e66220142f7
lavylipesh <lilibethlangat@yahoo.com>
2019-09-25 17:48:39 -0400

    add comment class

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 from.config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 bootstrap = Bootstrap()
@@ -14,4 +19,8 @@ def create_app(config_name):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lilibeth:1234@localhost/tech'
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     return app
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import User,Message
 from flask_migrate import Migrate,MigrateCommand
 
 app=create_app('production')
@@ -12,7 +12,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Message=Message)
 
 if __name__ =='__main__':
     manager.run()
